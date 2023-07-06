5f3f2ca35bb2bd650517109ac02e080beef41f35
Vlad Zanko <vlad.zanko.2019@mail.ru>
2023-07-06 19:10:49 +0300

    completed app

--- src/App.js
+++ src/App.js
@@ -4,21 +4,43 @@ import { ToDoItem } from './components/ToDoItem/ToDoItem';
 import './App.css';
 
 function App() {
-  const [tasks, setTask] = useState([]);
+  const [tasks, setTask] = useState(window.localStorage.getItem('items') ? JSON.parse(window.localStorage.getItem('items')) : []);
   const addTask = (text) => {
     if(text){
       let item = {
         id: tasks.length,
-        text: text
+        text: text,
+        completed: false
       }
-      setTask([...tasks, item]);
+      let newState = [...tasks, item];
+      window.localStorage.setItem('items', JSON.stringify(newState));
+      setTask(newState);
       console.log(item)
     }
     
   }
   const deleteTask = (id) => {
-    setTask([...tasks.filter((e)=> e.id !== id)]);
+    let newState = [...tasks.filter((e)=> e.id !== id)];
+    window.localStorage.setItem('items', JSON.stringify(newState));
+    setTask(newState);
   }
+  
+  const completed = (id) => {
+    let newState = [...tasks.filter((e)=> e.id === id ? {...e.completed = !e.completed}  : e)]; 
+    //! Обязательны {} так как нужно если e.id === id вернуть именно объект, без них мы просто изменим свойство, а самого объекта не будет
+    window.localStorage.setItem('items', JSON.stringify(newState));
+    setTask(newState);
+  }
+  // const completed = (id) => {
+  //   let newState = tasks.filter((e)=>{
+  //     if(e.id === id){
+  //       e.completed = !e.completed;
+  //     } 
+  //     return e;
+  //   });
+  //   window.localStorage.setItem('items', JSON.stringify(newState));
+  //   setTask(newState);
+  // }
   return (
     
     <div className='toDoAPP'>
@@ -26,10 +48,10 @@ function App() {
       <div className='container'>
           <ToDoForm setTask = {addTask}/>
         <hr style={{marginBottom : 15}}/>
-        {tasks.map((todo) => <ToDoItem todo = {todo} id = {todo.id}  key = {todo.id} deleteTask = {deleteTask}/>)}
+        {tasks.map((todo) => <ToDoItem todo = {todo} id = {todo.id} toggleComplete = {completed} key = {todo.id} deleteTask = {deleteTask}/>)}
       </div>
     </div>
-
+    
   );
 }
 
--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -1,8 +1,9 @@
 import './ToDoItem.css'
 export const ToDoItem = (props) => {
+    
     return(
         <div className='todo-item'>
-            <p >{props.todo.text}</p>
+            <p onClick = {()=>{props.toggleComplete(props.id)}} className = {props.todo.completed === true ? 'completed' : '' }>{props.todo.text}</p>
             <button onClick={()=>{props.deleteTask(props.id)}}>x</button>
         </div>
     )
